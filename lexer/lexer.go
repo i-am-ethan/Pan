ece092821f8dfd0e19fa4c432483b735c6f97572
i-am-ethan <izawaryosuke0101@gmail.com>
2025-11-30 23:27:50 +0900

    不要なコメント削除

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,7 +9,6 @@ func Lexer(input string) {
 	fmt.Printf("lexer input: %s\n", input)
 
 	for i, ch := range input {
-		// fmt.Printf("position: %d, ch: %c\n", i, ch)
 		switch {
 		case unicode.IsDigit(ch):
 			fmt.Printf("position: %d, %c is digit\n", i, ch)
